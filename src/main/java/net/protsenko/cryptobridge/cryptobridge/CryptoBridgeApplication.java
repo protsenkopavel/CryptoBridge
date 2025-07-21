d5942ef20374158b6460f19774f90f5c610ce299
Pavel Protsenko <pavelprotsenko@outlook.com>
2025-07-21 23:50:36 +0300

    init commit

---
+++ src/main/java/net/protsenko/cryptobridge/cryptobridge/CryptoBridgeApplication.java
@@ -0,0 +1,13 @@
+package net.protsenko.cryptobridge.cryptobridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoBridgeApplication.class, args);
+    }
+
+}
