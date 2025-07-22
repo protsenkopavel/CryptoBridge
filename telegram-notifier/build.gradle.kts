8440a6acaa1a7957b1a7d4fc896177e7a9dd2c81
Pavel Protsenko <pavelprotsenko@outlook.com>
2025-07-22 04:01:30 +0300

    telegram-notifier init commit

---
+++ telegram-notifier/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "net.protsenko.cryptobridge"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(24)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.telegram:telegrambots:6.7.0")
+	implementation("org.telegram:telegrambots-spring-boot-starter:6.7.0")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ telegram-notifier/src/main/java/net/protsenko/cryptobridge/telegramnotifier/TelegramNotifierApplication.java
@@ -0,0 +1,13 @@
+package net.protsenko.cryptobridge.telegramnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramNotifierApplication.class, args);
+	}
+
+}
